31515d3340be2cfb5530b1a612064ca1e8190439
beltmkt <alissoncorreia31@gmail.com>
2025-10-15 03:44:52 -0300

    Update OnBotChat.tsx
    Signed-off-by: beltmkt <alissoncorreia31@gmail.com>

--- src/components/OnBotChat.tsx
+++ src/components/OnBotChat.tsx
@@ -149,7 +149,7 @@ export const OnBotChat: React.FC<OnBotChatProps> = ({ onClose }) => {
       <div className="flex items-center justify-between p-3 border-b border-gray-700 bg-gray-900 rounded-t-lg">
         <div className="flex items-center gap-2">
           <img 
-            src="onbot-avatar.jpg" 
+            src="onbot-avatar.jpeg" 
             alt="OnBot"
             className="w-6 h-6 rounded"
           />
